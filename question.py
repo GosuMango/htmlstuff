2a554628356e8cd093e35fe362b7a61f781673f0
GosuMango <myu70@stuy.edu>
2025-03-12 14:48:41 -0400

    Add files via upload

---
+++ question.py
@@ -0,0 +1,22 @@
+def toaddall (string):
+    totalvalue = 0
+    c = 0
+    while c < len(string):
+        if ord(string[c]) == 32 or ord(string[c]) < 65 or ord(string[c]) > 122:
+            c+= 1
+        else:
+            totalvalue = totalvalue + ord(string[c])
+            c+= 1
+    return(totalvalue)
+    
+
+print( 'N o : ' + str(toaddall('N o') ))
+# This should result in 189.
+print( 'erm what the sigma : ' + str(toaddall('erm what the sigma') ))
+# This should result in 1610.
+print( 'Massive : ' + str(toaddall('Massive') ))
+# This should result in 728.
+print( 'Massive! : ' + str(toaddall('Massive!') ))
+# This should also result in 728.
+print( '!()#&(!)A@921009 : ' + str(toaddall('!()#&(!)A@921009') ))
+# This should result in 65.
